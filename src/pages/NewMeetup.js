1d371c3143a4e44a57f050eedcbf93fe9de211a9
Agarwal <2194200@cognizant.com>
2022-10-05 01:29:19 -0400

    useeffect

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,28 @@
-function NewMeetups(){
-    return<div>New Meet Up</div>
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import {useNavigate} from 'react-router-dom';
+
+function NewMeetups() {
+const navigate = useNavigate();
+
+  function addMeetupHandler(meetupData) {
+    fetch("https://react-acad-default-rtdb.firebaseio.com/meetups.json", {
+     method :'POST',
+     body:JSON.stringify(meetupData),
+     headers:{
+        'Content-Type': 'application/json'
+     }
     }
-    
-    export default NewMeetups;
+    ).then(()=>{
+        navigate('/');
+    });
+  }
+
+  return (
+    <section>
+      <h1>New Meet Up</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetups;
